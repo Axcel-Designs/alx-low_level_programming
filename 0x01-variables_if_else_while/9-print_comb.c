8759cd83f4eb8683ca6a1a6aa6256e3c8efb0e3c
axcel <axceldesigns@gmail.com>
2023-09-10 22:25:24 -0700

    variables, if  else and while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point 
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = 0; comb <= 9; comb++)
+	{
+		putchar(comb + '0');
+		if (comb < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
