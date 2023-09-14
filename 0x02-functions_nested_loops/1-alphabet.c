5a96f0ee39026e7b3aaff7d8cace2d5af6adfa76
axcel <axceldesigns@gmail.com>
2023-09-13 21:57:43 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+i/**
+* main - entry point
+* return : 0 always
+*/
+void print_alphabet(void)
+{
+	int alph
+	for (alph = a; alph <= z; alph++)
+		_putchar(alph);
+	_putchar('\n')
+}
