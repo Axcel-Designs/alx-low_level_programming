aac974daac3a2a3314340b00a5795500104516ec
axcel <axceldesigns@gmail.com>
2023-09-16 11:06:37 -0700

    loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - compute the absolute value of an integar
+ *
+ * Return: void
+ */
+int _abs(int c)
+{
+	if (c > 0)
+		return (c);
+	else
+		return (-c);
+}
